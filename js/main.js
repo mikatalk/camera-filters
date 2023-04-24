459dd2935f6d87f2629f93aee422aabe1bf51274
Mikatalk <mikatalk@users.noreply.github.com>
2023-04-23 23:00:23 -0700

    Add deps

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 // https://github.com/vinooniv/video-bg-blur/
 // https://blog.francium.tech/edit-live-video-background-with-webrtc-and-tensorflow-js-c67f92307ac5
 import * as THREE from 'three';
-import Stats from 'three/addons/libs/stats.module.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
